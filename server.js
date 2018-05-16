3bbdc261002932f2b6a5eaf30a739497279d7767
Ayeshkant Mishra <ayeshkant.mishra7@gmail.com>
2018-05-17 00:04:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express=require('express');
+//loading handlebars
+const hbs=require('hbs');
+const fs = require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if (err) {
+      console.log('Unable to append file');
+    }
+  });
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintanance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle:'Welcome Home',
+
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About page',
+
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage:'Error handling the message'
+  });
+});
+
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000');
+});
